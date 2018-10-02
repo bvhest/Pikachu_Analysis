9b12aff92dc522e18bf964702e7b6f91e84dc196
Hest <bart.van.hest_1@philips.com>
2018-10-02 16:11:23 +0200

    Channel configuration (runtime parameters and configured locales) queried from the Pikachu database and processed into the wide spredsheet format.

--- code/queryChannelDBconfiguration.R
+++ code/queryChannelDBconfiguration.R
@@ -48,35 +48,151 @@ library(RJDBC)
   
   
 # Create connection driver and open connection (define full path on Windows...)
-jdbcDriver <- 
-  JDBC(driverClass="oracle.jdbc.OracleDriver", 
-       classPath="c:/Java/lib/ojdbc8.jar")
+oracle.JdbcDriver <- 
+  RJDBC::JDBC(driverClass="oracle.jdbc.OracleDriver", 
+              classPath="c:/Java/lib/ojdbc8.jar")
 
-jdbcConnection <- 
-  dbConnect(jdbcDriver, 
-            "jdbc:oracle:thin:@gdcocl.gdc1.ce.philips.com:5323:PCHU1Q", 
-            "BATCH", 
-            "hbakip")
+qa.jdbcConnection <- 
+  DBI::dbConnect(drv = oracle.JdbcDriver,
+                 url = "jdbc:oracle:thin:@130.139.136.68:1521:PCHU1Q",
+                 user = "CMC2_QA1_SCHEMA", 
+                 password = "CMC2_QA")
 
-jdbcConnection <- 
-  dbConnect(jdbcDriver, 
-            "jdbc:oracle:thin:@gdcocl.gdc1.ce.philips.com:5334:PCHU1P", 
-            "cmcprd1", 
-            "1drpcmc")
+pr.jdbcConnection <- 
+  DBI::dbConnect(drv = oracle.JdbcDriver,
+                 url = "jdbc:oracle:thin:@130.139.136.69:1521:PCHU2P", 
+                 user = "U1BATCH", 
+                 password = "CMC2_QA")
 
 # Query on the Oracle instance name.
-instanceName <- 
-  dbGetQuery(jdbcConnection, "SELECT instance_name FROM v$instance")
-print(instanceName)
+channels.r.df <- 
+  DBI::dbGetQuery(conn = qa.jdbcConnection, 
+                  statement = "SELECT * FROM CMC2_QA1_SCHEMA.CHANNELS WHERE MACHINEAFFINITY != 'deprecated' AND ch.TYPE = 'export'" )
+
+# 3110 # EloquaProducts
+ch_locales.r.df <- 
+  DBI::dbGetQuery(conn = qa.jdbcConnection, 
+                  statement = "SELECT CUSTOMER_ID, LOCALE, ENABLED, LOCALEENABLED FROM CMC2_QA1_SCHEMA.CHANNEL_CATALOGS WHERE CUSTOMER_ID = 3110 AND CATALOG_TYPE= 'CONSUMER'" )
+
+# all channels
+qry <- "SELECT ch.NAME,
+       cc.LOCALE, 
+       cc.ENABLED, 
+       cc.LOCALEENABLED
+FROM CMC2_QA1_SCHEMA.CHANNELS ch
+INNER JOIN CMC2_QA1_SCHEMA.CHANNEL_CATALOGS cc
+   ON cc.CUSTOMER_ID = ch.ID
+WHERE ch.MACHINEAFFINITY != 'deprecated'
+  AND ch.TYPE = 'export'
+  AND cc.CATALOG_TYPE= 'CONSUMER'"
+
+ch_locales.r.df <- 
+  DBI::dbGetQuery(conn = qa.jdbcConnection, 
+                  statement = qry)
 
 # Close connection
-dbDisconnect(jdbcConnection)
+dbDisconnect(qa.jdbcConnection)
 
-con <- DBI::dbConnect(odbc::odbc(),
-                      Driver = "[your driver's name]",
-                      Host   = "[your server's path]",
-                      SVC    = "[your schema's name]",
-                      UID    = rstudioapi::askForPassword("Database user"),
-                      PWD    = rstudioapi::askForPassword("Database password"),
-                      Port   = 1521)
+save(channels.r.df,
+     ch_locales.r.df,
+     file = "./code/data/db_channel_config.RData")
+
+###############################################################################
+# data cleaning & manipulation
+###############################################################################
+
+#
+# channel configuration
+#
+
+# check parameter-list
+channels.params.df <-
+  channels.r.df %>%
+  # keep exports (remove other processes)
+  dplyr::filter(TYPE == "export") %>%
+  # remove irrelevant columns
+  dplyr::select(NAME, PIPELINE, CATALOG) %>%
+  # split process and parameters
+  tidyr::separate(col = PIPELINE, 
+                  into = c('BaseExport', 'Parameters'), 
+                  sep = "\\?",
+                  fill = "right",
+                  remove = TRUE) %>%
+  # get BaseExport from process triggering url:
+  dplyr::mutate(BaseExport = stringr::str_remove(string = BaseExport,
+                                                 pattern = "pipes/"),
+                BaseExport = dplyr::if_else(stringr::str_detect(string = BaseExport, 
+                                                                pattern = "\\."),
+                                            stringr::str_remove(string = BaseExport,
+                                                                pattern = paste0(".", NAME)),
+                                            BaseExport)) %>%
+  # split parameters
+  tidyr::separate(col = Parameters, 
+                  into = c('par1', 'par2', 'par3', 'par4','par5', 'par6', 'par7', 'par8'), 
+                  sep = "&",
+                  fill = "right",
+                  remove = TRUE) %>%
+  # from wide to long format
+  tidyr::gather(key = remove,
+                value = parameter, 
+                na.rm = TRUE,
+                par1:par8) %>%
+  # remove unnecesarry columns
+  dplyr::select(-remove) %>%
+  # split parameter name and value
+  tidyr::separate(col = parameter, 
+                  into = c('parameter', 'value'), 
+                  sep = "=",
+                  fill = "right",
+                  remove = TRUE) %>%
+  # convert channel name to lower-case:
+  dplyr::rename(channel = NAME,
+                catalog = CATALOG) %>%
+  # sort alphabetically
+  dplyr::arrange(channel, parameter)
+
+glimpse(channels.params.df)
+
+# transpose into (wide) tabel/spreadsheet format
+channels.params.wide.df <-
+  channels.params.df %>%
+  tidyr::spread(key = parameter,
+                value = value)
+
+# note: the same paramters ae present in both lower- and title-case !!
+
+#
+# locales per channel configuration
+#
+channels.locales.df <-
+  ch_locales.r.df %>%
+  # remove all rows that are dsabled:
+  dplyr::filter(LOCALEENABLED == 1) %>%
+  # remove irrelevant columns
+  dplyr::select(NAME, LOCALE, LOCALEENABLED) %>%
+  # convert column names:
+  dplyr::rename(channel = NAME,
+                locale = LOCALE,
+                enabled = LOCALEENABLED) %>%
+  # remove duplicate entries(duplicate becasue??)
+  dplyr::distinct(channel, locale, .keep_all = TRUE) %>%
+  # order data
+  dplyr::arrange(channel, locale)
+
+# transpose into (wide) tabel/spreadsheet format
+channels.locales.wide.df <-
+  channels.locales.df %>%
+  tidyr::spread(key = channel,
+                value = enabled)
+
+###############################################################################
+# export processed data (in csv-format suitable for spreadsheet)
+###############################################################################
+channels.params.wide.df %>%
+  readr::write_excel_csv(path="./analyse/output/channelParameters.csv",
+                         na = "")
+
+channels.locales.wide.df %>%
+  readr::write_excel_csv(path="./analyse/output/channelLocales.csv",
+                         na = "")
 
