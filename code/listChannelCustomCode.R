5b144bf17696d7f3c1f8056b1994ca2098eb7774
Hest <bart.van.hest_1@philips.com>
2018-10-02 17:10:48 +0200

    Added code to: 1. list obsolete channels (code-directories can be removed) 2. automated file-diff.

--- code/listChannelCustomCode.R
+++ code/listChannelCustomCode.R
@@ -11,7 +11,7 @@ library(tidyverse)
 # read data
 ###############################################################################
 
-custom_code.r <-
+code.r <-
   list.files(path = "./webapp.b2c/pipes/ProductExport/xsl",
              pattern = "*.xsl",
              full.names = TRUE,
@@ -27,8 +27,8 @@ activeChannels.df <-
 #   - sorted alphabetically
 ###############################################################################
 
-custom_code.df <-
-  custom_code.r %>%
+code.df <-
+  code.r %>%
   # map to data-frame and provide name for nameless column:
   as.data.frame(stringsAsFactors = FALSE) %>%
   dplyr::select(files = 1) %>%
@@ -40,8 +40,18 @@ custom_code.df <-
                   into = c('channel', 'file'), 
                   sep = "/",
                   fill = "left",
-                  remove = TRUE) %>%
-  # filter out the process base stylesheets, keeping only the export specific code:
+                  remove = TRUE)
+
+base_code.df <-
+  code.df %>%
+  # filter out the channel specific code:
+  dplyr::filter(is.na(channel)) %>%
+  # sort alphabetically
+  dplyr::arrange(channel, file)
+
+custom_code.df <-
+  code.df %>%
+  # filter out the process base stylesheets, keeping only the channel specific code:
   dplyr::filter(!is.na(channel)) %>%
   # sort alphabetically
   dplyr::arrange(channel, file) %>%
@@ -58,9 +68,112 @@ custom_code.wide.df <-
   tidyr::spread(key = "channel",        # spread
                 value = "inUse")
 
+#
+# list custom-code for incactive channels: can be deleted from Pikachu code repo.
+#
+# example: AtgGifting
+#
+obsolete_code.df <-
+  code.df %>%
+  # filter out the process base stylesheets, keeping only the channel specific code:
+  dplyr::filter(!is.na(channel)) %>%
+  # only keep unique channels/directories (files are not important)
+  dplyr::distinct(channel) %>%
+  # sort alphabetically
+  dplyr::arrange(channel) %>%
+  # remove inactive channels/keep active channels
+  dplyr::anti_join(activeChannels.df, by = "channel")
+
+# code that can be deleted:
+glimpse(obsolete_code.df)
+
 ###############################################################################
 # export processed data (in csv-format suitable for spreadsheet)
 ###############################################################################
 custom_code.wide.df %>%
   readr::write_excel_csv(path="./analyse/output/channelCustomCode.csv",
                          na = "")
+
+obsolete_code.df %>%
+  readr::write_excel_csv(path="./analyse/output/obsoleteChannels_deleteCode.csv",
+                         na = "")
+
+###############################################################################
+# perform an automated diff between the base code and the channel custom code.
+#
+# see 
+#   - https://stackoverflow.com/questions/43081791/in-r-find-whether-two-files-differ
+#   - https://community.rstudio.com/t/comparing-files/2323/7
+#   - https://stackoverflow.com/questions/7811878/show-difference-between-files-or-objects
+###############################################################################
+library(diffobj)
+
+base_dir <- 
+  "./webapp.b2c/pipes/ProductExport/xsl"
+out_dir <- 
+  "./code/data/diff"
+# create output-dir (if non-existent)
+if (!dir.exists(out_dir)) {
+  dir.create(out_dir, 
+             showWarnings = FALSE)
+}
+  
+
+# loop over channels
+
+v_channel <- "EloquaProducts"
+print(v_channel) 
+# loop over custom code per channel
+for (v_file in custom_code.df %>%
+               dplyr::filter(channel == v_channel) %>%
+               dplyr::select(file)) {
+  v_file <- "convertProducts.xsl"
+  print(v_file)
+
+  base_file <-
+    base_code.df %>%
+    dplyr::filter(file == v_file) %>%
+    dplyr::select(file) %>%
+    dplyr::pull()
+  print(base_file)
+
+  # compare the custom-code with the base-code
+  v_target <-
+    paste(base_dir, v_channel, v_file, 
+          sep = "/")
+  v_current <-
+    paste(base_dir, base_file, 
+          sep = "/")
+  
+  filediff <-
+    diffobj::diffFile(target = v_target, 
+                      current = v_current)  
+  
+  # store the diff on the file-system:
+  v_output_dir <-
+    paste(out_dir, v_channel, 
+          sep = "/")
+
+  # create subdirectory, if required
+  if (!dir.exists(v_output_dir)) {
+    dir.create(v_output_dir, 
+               showWarnings = FALSE)
+  }
+  # write file
+  v_output_file <-
+    paste(v_output_dir, stringr::str_replace(v_file, 
+                                             pattern = ".xsl",
+                                             replace = ".txt"), 
+          sep = "/")
+
+  # readr::write_lines(x = as.character(filediff), 
+  #                    path = v_output_file, 
+  #                    append = FALSE)
+  write(x = as.character(filediff), 
+        file = v_output_file, 
+        append = FALSE)
+  
+} # END loop over custom code per channel
+
+
+
