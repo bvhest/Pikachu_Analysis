081a2ff324f528a316e921ce0bf502f9cc6286c9
hestbv <hestbv@gmail.com>
2018-12-05 12:36:40 +0100

    Added data with the SIM-tool ftp processes.

---
+++ code/135_FTP_processes.R
@@ -0,0 +1,18 @@
+## Missing SIM-tool FTP-processes for active exports
+#
+# BHE 2018-21-05
+#
+
+library(tidyverse)
+
+# load ftp-processes
+ftpProcesses.r <-
+  readr::read_delim(file = "./data/simtool/processes.txt", delim = "|", col_names = TRUE)
+
+activeExports <-
+  readRDS(file = "./analyse/output/activeChannels.Rds")
+
+# munge
+ftpProcesses.c <-
+  ftpProcesses.r %>%
+  dplyr::left_join(activeExports, by = c("channel" = "job"))
