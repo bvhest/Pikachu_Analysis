8dc0720e646c99504ac74dc7c2a9e982f9ea9141
hestbv <hestbv@gmail.com>
2019-06-05 12:48:17 +0200

    First conclusions on asset definitions and implementeation in STEP.

--- code/630_STEP_asset_consistency_check.R
+++ code/630_STEP_asset_consistency_check.R
@@ -11,5 +11,48 @@
 library(tidyverse)
 
 # load data
-readRDS(file = "./data/STEP_assets/asset.meta.spreadsheet.rds")
-readRDS(file = "./data/STEP_assets/asset.meta.step.rds")
+asset.metdata.def <- 
+  readRDS(file = "./data/STEP_assets/asset.meta.spreadsheet.rds")
+asset.metdata.impl <- 
+  readRDS(file = "./data/STEP_assets/asset.meta.step.rds")
+
+# how many unique assets?
+# defined
+asset.metdata.def %>%
+  dplyr::distinct(doctype) %>%
+  dplyr::count()
+# 197
+
+# implemented
+asset.metdata.impl %>%
+  dplyr::filter(type == "asset") %>%
+  dplyr::distinct(id) %>%
+  dplyr::count()
+# 119
+
+# implemented relations (= number of doctypes)
+asset.metdata.impl %>%
+  dplyr::filter(type == "asset") %>%
+  dplyr::distinct(doctype) %>%
+  dplyr::count()
+# 145
+
+# merge the two collections: 
+#   1. check missing implementations
+asset.metdata.def %>%
+  dplyr::select(doctype) %>%
+  dplyr::distinct(doctype) %>%
+  dplyr::anti_join(asset.metdata.impl, by = "doctype") %>%
+  dplyr::select(doctype) %>%
+  dplyr::distinct(doctype)
+# 45 doctypes defined, but not implemented in STEP...
+
+#   2. check implementations without definition
+asset.metdata.impl %>%
+  dplyr::select(doctype) %>%
+  dplyr::distinct(doctype) %>%
+  dplyr::anti_join(asset.metdata.def, by = "doctype") %>%
+  dplyr::select(doctype) %>%
+  dplyr::distinct(doctype)
+# 6 doctypes implemented, yet not defined
+
