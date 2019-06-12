2c02b3486ef102773221c3c1ae93fcbec831265b
hestbv <hestbv@gmail.com>
2019-06-12 10:49:24 +0200

    Made STEP processing code more generic in order to deal with the different development environments (dev/tst/uat/prd).

--- code/620_STEP_asset_consistency_preprocess_stepdata.R
+++ code/620_STEP_asset_consistency_preprocess_stepdata.R
@@ -3,62 +3,73 @@
 #   definitions based on spreadsheets provided by Sabine with the metadata 
 #   and an export from asset definitions from STEP.
 #
-# BHE, 29-05-2019
+# BHE, 29-05-2019, 11-06-2019
 #
 
 library(tidyverse)
 library(readr)
 
-
-###############################################################################
-# read asset metadata from to-csv-format converted STEP-export
-#
-###############################################################################
-
-asset.meta.r <-
-  readr::read_csv(file = "./data/csv/620_STEP_assets_metadata.csv")
-asset_specs.meta.r <-
-  readr::read_csv(file = "./data/csv/620_STEP_asset_specs_metadata.csv")
-
-glimpse(asset.meta.r)
-glimpse(asset_specs.meta.r)
-
-###############################################################################
-# data cleaning
-###############################################################################
-
-asset.reference.c <-
-  asset.meta.r %>%
-  # filter on asset-reference-type
-  dplyr::filter(type == 'asset-ref')
-
-asset.meta <-
-  asset.meta.r %>%
-  # filter on asset-type
-  dplyr::filter(type == 'asset') %>%
-  # add asset specs
-  dplyr::right_join(asset_specs.meta.r, by = c("type", "id")) %>%
-  # remove 'PHL' prefix
-  dplyr::mutate(spec_id = stringr::str_remove(stringr::str_remove(spec_id, "PH-"), "Asset-"))
-
-asset.meta.c <-
-  # join asset-metadata with asset-reference to
-  #   - get doctype (defined on the relation, not on the asset)
-  #   - get cardinality of the asset-assetref relation
-  #nb. prevent too many double names, so sub-select columns (needs an inversion of processing of the tables)
-  asset.reference.c %>%
-  dplyr::select(id, parentID) %>%
-  dplyr::rename(doctype = id) %>%
-  dplyr::right_join(asset.meta, by = c("parentID" = "id")) %>%
-  # clean-up mess of column names
-  dplyr::rename(id = parentID) %>%
-  dplyr::select(-parentID.y) %>%
-  # reshuffle order of columns
-  dplyr::select(type, id, doctype, everything())
-
-# save for future use:
-saveRDS(asset.meta.c, file = "./data/STEP_assets/asset.meta.step.rds")
-saveRDS(asset.reference.c, file = "./data/STEP_assets/asset-references.meta.step.rds")
+# process the data exported from the different STEP environments:
+for (env in c("dev" , "tst")) {
+  
+  ###############################################################################
+  # read asset metadata from "to-csv-format" converted STEP-export
+  #
+  ###############################################################################
+  
+  filename <- 
+    paste0("./data/csv/",env,"/620_STEP_assets_metadata.csv")
+  asset.meta.r <-
+    readr::read_csv(file = filename)
+  filename <- 
+    paste0("./data/csv/",env,"/620_STEP_asset_specs_metadata.csv")
+  asset_specs.meta.r <-
+    readr::read_csv(file = filename)
+  
+  glimpse(asset.meta.r)
+  glimpse(asset_specs.meta.r)
+  
+  ###############################################################################
+  # data cleaning
+  ###############################################################################
+  
+  asset.reference.c <-
+    asset.meta.r %>%
+    # filter on asset-reference-type
+    dplyr::filter(type == 'asset-ref')
+  
+  asset.meta <-
+    asset.meta.r %>%
+    # filter on asset-type
+    dplyr::filter(type == 'asset') %>%
+    # add asset specs
+    dplyr::right_join(asset_specs.meta.r, by = c("type", "id")) %>%
+    # remove 'PHL' prefix
+    dplyr::mutate(spec_id = stringr::str_remove(stringr::str_remove(spec_id, "PH-"), "Asset-"))
+  
+  asset.meta.c <-
+    # join asset-metadata with asset-reference to
+    #   - get doctype (defined on the relation, not on the asset)
+    #   - get cardinality of the asset-assetref relation
+    #nb. prevent too many double names, so sub-select columns (needs an inversion of processing of the tables)
+    asset.reference.c %>%
+    dplyr::select(id, parentID) %>%
+    dplyr::rename(doctype = id) %>%
+    dplyr::right_join(asset.meta, by = c("parentID" = "id")) %>%
+    # clean-up mess of column names
+    dplyr::rename(id = parentID) %>%
+    dplyr::select(-parentID.y) %>%
+    # reshuffle order of columns
+    dplyr::select(type, id, doctype, everything())
+  
+  # save for future use:
+  filename <- 
+    paste0("./data/STEP_assets/",env,"/asset.meta.step.rds")
+  saveRDS(asset.meta.c, file = filename)
+  filename <- 
+    paste0("./data/STEP_assets/",env,"/asset-references.meta.step.rds")
+  saveRDS(asset.reference.c, file = filename)
+}
 
 # DONE
 #
--- code/620_STEP_asset_xml2csv.bat
+++ code/620_STEP_asset_xml2csv.bat
@@ -13,5 +13,8 @@ REM # BvH, 2019-29-05
 REM #
 
 REM # asset & asset-reference meta-data :
-java -cp C:\Java\Saxon/saxon9he.jar net.sf.saxon.Transform -t -s:../data/STEP_assets/STEP_Dev_asset_definitions_20190529.xml -xsl:620_STEP_asset_xml2csv.xsl -o:../data/csv/620_STEP_assets_metadata.csv
-java -cp C:\Java\Saxon/saxon9he.jar net.sf.saxon.Transform -t -s:../data/STEP_assets/STEP_Dev_asset_definitions_20190529.xml -xsl:620_STEP_asset_specs_xml2csv.xsl -o:../data/csv/620_STEP_asset_specs_metadata.csv
+java -cp C:\Java\Saxon/saxon9he.jar net.sf.saxon.Transform -t -s:../data/STEP_assets/dev/STEP_asset_definitions_20190612.xml -xsl:620_STEP_asset_xml2csv.xsl -o:../data/csv/dev/620_STEP_assets_metadata.csv
+java -cp C:\Java\Saxon/saxon9he.jar net.sf.saxon.Transform -t -s:../data/STEP_assets/dev/STEP_asset_definitions_20190612.xml -xsl:620_STEP_asset_specs_xml2csv.xsl -o:../data/csv/dev/620_STEP_asset_specs_metadata.csv
+
+java -cp C:\Java\Saxon/saxon9he.jar net.sf.saxon.Transform -t -s:../data/STEP_assets/tst/STEP_asset_definitions_20190612.xml -xsl:620_STEP_asset_xml2csv.xsl -o:../data/csv/tst/620_STEP_assets_metadata.csv
+java -cp C:\Java\Saxon/saxon9he.jar net.sf.saxon.Transform -t -s:../data/STEP_assets/tst/STEP_asset_definitions_20190612.xml -xsl:620_STEP_asset_specs_xml2csv.xsl -o:../data/csv/tst/620_STEP_asset_specs_metadata.csv
